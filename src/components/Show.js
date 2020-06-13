d12a8a1b292ef6425f64468096a6e6479af74ce5
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-13 18:19:55 -0500

    Show initial version

---
+++ src/components/Show.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// Core
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+// Utils
+import {genericStyles} from '../utils';
+
+function Show({name, poster}) {
+  return (
+    <View style={[styles.showContainer, styles.centerContents]}>
+      <Image source={{uri: poster}} style={styles.imageStyle} />
+      <Text>{name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  ...genericStyles,
+  imageStyle: {
+    height: 295,
+    width: 210,
+  },
+  showContainer: {
+    paddingVertical: 15,
+  },
+});
+
+export default Show;
