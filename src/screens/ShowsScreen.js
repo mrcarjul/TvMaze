755bc3954563815d7896a2354784fe8d13541ea2
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-13 14:45:52 -0500

    added genericStyles

--- src/screens/ShowsScreen.js
+++ src/screens/ShowsScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
+
+// Core
 import {StyleSheet, Text, View} from 'react-native';
+
+// Utils
+import {genericStyles} from '../utils';
 
 /**
  * @description The purpose of the screen is to be the main screen to display shows list
  */
 function ShowsScreen() {
   return (
-    <View style={styles.mainContainer}>
+    <View style={styles.container}>
       <Text>Shows Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  ...genericStyles,
   mainContainer: {
     flex: 1,
   },
