75fb5db1328caab6d819c88d1db1d3c95434d3a8
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-14 23:31:21 -0500

    initial version of metrics

---
+++ src/utils/metrics.js
@@ -0,0 +1,18 @@
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+const metrics = {
+  imageRatio: 0.71,
+  height,
+  icon: 24,
+  margin: 14,
+  paddingLeft: 12,
+  marginHorizontal: 10,
+  marginVertical: 20,
+  radius: 7,
+  section: 50,
+  width,
+};
+
+export default metrics;
