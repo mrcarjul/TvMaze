a7928aace9a8e78b9c36e49a85712897792f49c8
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-14 20:25:38 -0500

    performance improvements added

--- src/screens/ShowsScreen.js
+++ src/screens/ShowsScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 
 // Core
 import {FlatList, StyleSheet, Text, View} from 'react-native';
@@ -18,7 +18,7 @@ const ITEM_HEIGHT = metrics.width / 2 / metrics.imageRatio + 150;
 /**
  * @description The purpose of the screen is to be the main screen to display shows list
  */
-function ShowsScreen() {
+function ShowsScreen({navigation}) {
   const flatListRef = useRef(null);
   const {error, errorMsg, fetching, shows, page} = useSelector(
     state => state.shows,
@@ -28,13 +28,32 @@ function ShowsScreen() {
   const colors = getThemeColors(themeColorType);
   const {textStyle} = fonts;
 
-  const requestShows = () => {
+  const requestShows = useCallback(() => {
     dispatch(getShowsByPageAction(0));
-  };
+  }, [dispatch]);
 
-  const requestMoreShows = () => {
+  const requestMoreShows = useCallback(() => {
     dispatch(getShowsByPageAction(page));
-  };
+  }, [dispatch, page]);
+
+  const getItemLayout = useCallback((data, index) => {
+    return {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index};
+  }, []);
+
+  const getKey = ({id}) => id;
+
+  const renderItem = useCallback(
+    ({item}) => (
+      <Show
+        id={item.id}
+        colors={colors}
+        name={item.name}
+        poster={item.image}
+        navigation={navigation}
+      />
+    ),
+    [colors, navigation],
+  );
 
   /**
    * @description on Screen Mount request shows
@@ -43,29 +62,26 @@ function ShowsScreen() {
 
   return (
     <View style={[styles.container, {backgroundColor: colors.backgroundAlt}]}>
-      <Text style={textStyle.centeredTitle}>Shows</Text>
+      <Text style={textStyle.centeredTitle}>Series</Text>
       <SearchInput />
       <View style={styles.container}>
         <FlatList
-          ref={flatListRef}
-          initialNumToRender={10}
-          maxToRenderPerBatch={10}
           data={shows}
-          keyExtractor={show => show.id}
-          renderItem={({item}) => (
-            <Show
-              index={item.id}
-              colors={colors}
-              name={item.name}
-              poster={item.image}
-            />
-          )}
+          extraData={shows}
+          getItemLayout={getItemLayout}
+          initialNumToRender={10}
+          keyExtractor={getKey}
+          maxToRenderPerBatch={10}
           onEndReached={requestMoreShows}
-          getItemLayout={(data, index) => {
-            return {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index};
-          }}
+          renderItem={renderItem}
+          ref={flatListRef}
           windowSize={5}
         />
+        {error && (
+          <View style={[styles.container, styles.centerContents]}>
+            <Text style={textStyle.normal}>{errorMsg}</Text>
+          </View>
+        )}
         <ListLoader fetching={fetching} />
         <ScrollToTopFab flatListRef={flatListRef} />
       </View>
