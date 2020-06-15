b2368d4f0a13061b09f56b0fe33e2e535236f967
Carlos <carlos.martinez@obsidiansoft.io>
2020-06-15 14:05:05 -0500

    added default images, sometimes not available

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,8 @@
 // precache images(improves performance on render)
 const images = {
+  episode_image: require('./no_episode_image.png'),
   logo: require('./logo.png'),
-  noimage: require('./noimage.png'),
+  show_image: require('./no_show_image.png'),
 };
 
 export default images;
